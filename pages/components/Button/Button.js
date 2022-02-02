fede5f8b6ba759491703b6ac2c00712710067610
Yassine Chih <yassinechih7@gmail.com>
2022-02-03 00:35:28 +0100

    newstuff

--- pages/components/Button/Button.js
+++ pages/components/Button/Button.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './Button.module.css';
 
 function Button(props) {
-  return <div className={styles.Button}>
+
+  const [buttonState, toggleButton] = useState(1);
+  const [buttonMode, setButtonMode] = useState("active");
+
+  const handleToggle = () => {
+    buttonState ? toggleButton(0): toggleButton(1)  
+    buttonMode == "active" ? setButtonMode("disabled"): setButtonMode("active")  
+  }
+
+
+  return (
+  <div className={styles.Button +" "+ styles.buttonMode}>
       <button
 
       name={props.name}
       type={props.type}
-      onClick={props.OnClick}
+      onClick={() => {
+        props.OnClick
+        handleToggle()
+      }}
       >
       {props.text}
+
       </button>
-  </div>;
-}
+  </div>
+  )}
 
 export default Button;
